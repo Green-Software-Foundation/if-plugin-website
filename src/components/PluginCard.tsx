4b6dc47060249fcb5ba680ec3992f6d9348c9d0a
Osama Jandali <osamajandaly@gmail.com>
2024-06-24 12:33:12 +0300

    Misc fixes

--- src/components/PluginCard.tsx
+++ src/components/PluginCard.tsx
@@ -33,7 +33,12 @@ const ProfileCard = ({
     <div className="w-full h-full p-6 bg-white rounded-lg shadow transition-all relative z-10 overflow-hidden inline-block  ">
       <div className="flex justify-between flex-col md:flex-row mb-2 gap-2 items-start">
         <div className="order-2 md:order-none">
-          <h2 className="text-primary-default font-extrabold text-xl">{name}</h2>
+          <a href={npm || github || docs || "#"}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2 className="text-primary-default font-extrabold text-xl">{name}</h2>
+          </a>
           <h3 className="text-black-default text-sm font-semibold">by {author}</h3>
         </div>
         {badge && <div className="flex items-center gap-2 order-1 md:order-none w-full md:w-auto">
@@ -50,26 +55,26 @@ const ProfileCard = ({
       <hr className="my-6 bg-gray-default border-none h-0.5" />
       <div className="flex justify-center md:justify-between items-center flex-wrap gap-4 md:gap-0">
         <div className="flex items-center gap-4">
-          <a href={npm}
+          {npm && <a href={npm}
             target="_blank"
             rel="noopener noreferrer"
             className="w-10 text-gray-500 hover:text-primary-default transition-colors flex items-center justify-center" dangerouslySetInnerHTML={{ __html: NpmIcon }}>
-          </a>
-          <a href={github} target="_blank" rel="noopener noreferrer" className="w-6 text-gray-500 hover:text-primary-default transition-colors flex items-center justify-center" dangerouslySetInnerHTML={{ __html: GithubIcon }}>
-          </a>
-          <a href={docs} target="_blank" rel="noopener noreferrer" className="w-6 text-gray-500 hover:text-primary-default transition-colors flex items-center justify-center" >
+          </a>}
+          {github && <a href={github} target="_blank" rel="noopener noreferrer" className="w-6 text-gray-500 hover:text-primary-default transition-colors flex items-center justify-center" dangerouslySetInnerHTML={{ __html: GithubIcon }}>
+          </a>}
+          {docs && <a href={docs} target="_blank" rel="noopener noreferrer" className="w-6 text-gray-500 hover:text-primary-default transition-colors flex items-center justify-center" >
             <LinkIcon />
-          </a>
+          </a>}
         </div>
         <div className="flex justify-center flex-wrap gap-4">
-          <div>
+          {npm && <div>
             <span className="text-xs font-bold bg-black-default p-2 text-white rounded-bl rounded-tl ">NPM Weekly Download</span>
             <span className="text-xs font-bold bg-accent-default p-2 text-black-default rounded-br rounded-tr">{npmDownloads}</span>
-          </div>
-          <div>
+          </div>}
+          {github && <div>
             <span className="text-xs font-bold bg-black-default p-2 text-white rounded-bl rounded-tl ">Github Stars</span>
             <span className="text-xs font-bold bg-accent-default p-2 text-black-default rounded-br rounded-tr">{githubStars}</span>
-          </div>
+          </div>}
         </div>
       </div>
     </div>
